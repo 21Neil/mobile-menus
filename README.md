6caffb22f593c41cbda1fe2b89fd273071c26106
21Neil <88711171+21Neil@users.noreply.github.com>
2024-04-19 13:35:21 +0800

    Finish project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# ESLint-Webpack
+# Mobile Menus
+製作行動裝置的動態menus,加上一點CSS動畫
+## Demo
+
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,17 @@
 const app = () => {
-  console.log('hello world')
+  const menusBtnContainer = document.querySelector('.menus-btn-container')
+  
+  menusBtnContainer.addEventListener('click', menusContainerBtnOnClick)
+  
+  function menusContainerBtnOnClick() {
+    const menusBtn = document.querySelector('.menus-btn')
+    const closeMenusBtn = document.querySelector('.close-menus-btn')
+    const menus = document.querySelector('nav')
+
+    menusBtn.classList.toggle('opacity-0')
+    closeMenusBtn.classList.toggle('opacity-1')
+    menus.classList.toggle('slide')
+  }
 }
 
 export default app
